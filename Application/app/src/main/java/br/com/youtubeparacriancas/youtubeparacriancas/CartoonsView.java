4881bdab098a7bb8d771ba8aba2a40ce26e9174a
RenatoDev <renatomsilva@gmail.com>
2016-01-27 01:10:39 -0200

    MindMap - FreeMInd

---
+++ Application/app/src/main/java/br/com/youtubeparacriancas/youtubeparacriancas/CartoonsView.java
@@ -0,0 +1,13 @@
+package br.com.youtubeparacriancas.youtubeparacriancas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CartonsView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cartons_view);
+    }
+}
